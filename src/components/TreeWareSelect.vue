3d0f937dcd0ca3c99e448d5eaebc0493f180de6a
Deepak Nulu <4168103+deepak-nulu@users.noreply.github.com>
2019-10-31 12:34:16 -0700

    A searchable select component.

---
+++ src/components/TreeWareSelect.vue
@@ -0,0 +1,243 @@
+<template>
+  <div class="tree-ware-select" :class="{'show-options': showOptions}">
+    <input
+      ref="inputField"
+      type="text"
+      v-model="searchText"
+      @input="searchTextChanged"
+      @click="inputFocussed()"
+      @focus="inputFocussed()"
+      @blur="inputBlurred()"
+      @keydown.down="highlightDown()"
+      @keydown.up="highlightUp()"
+      @keydown.enter="handleInputEnter()"
+    />
+    <table class="tree-ware-select-options">
+      <tr
+        v-for="(option, index) in matchingOptions"
+        :key="getDisplayName(option)"
+        @mousedown.prevent
+        @click="optionSelected(option)"
+        class="tree-ware-select-option-row"
+        :class="{'highlight': index === highlightIndex}"
+      >
+        <!-- TODO(deepaknulu): selection indicator -->
+        <td class="selection-indicator">&nbsp;</td>
+        <td class="display-name">{{getDisplayName(option)}}</td>
+      </tr>
+    </table>
+  </div>
+</template>
+
+<script lang="ts">
+import "reflect-metadata";
+import {
+  Component,
+  Emit,
+  Prop,
+  PropSync,
+  Ref,
+  Vue,
+  Watch
+} from "vue-property-decorator";
+
+export type GetDisplayNameFunction = (option: Object | String) => string;
+export type FilterOptionsFunction = (
+  searchText: string,
+  options: Array<Object | String>
+) => Array<Object | String>;
+export type CreateOptionFunction = (input: string) => Object | String;
+
+@Component
+export default class TreeWareSelect extends Vue {
+  // TODO(deepaknulu): implement exactMatch = false functionality.
+  @Prop({ default: false }) exactMatch!: boolean;
+
+  /** Auto-selects first option. Applicable only if exactMatch is true. */
+  @Prop({ default: false }) autoSelectOnBlur!: boolean;
+
+  @PropSync("value", { type: [Object, String] }) syncedValue!: Object | String;
+  @Prop({ default: () => [] }) options!: Array<Object | String>;
+
+  @Prop({ default: defaultGetDisplayName })
+  getDisplayName!: GetDisplayNameFunction;
+
+  @Prop({ default: defaultFilterOptions })
+  filterOptions!: FilterOptionsFunction;
+
+  /** A function to create an option object from a user's input string */
+  @Prop({ default: defaultCreateOption }) createOption!: CreateOptionFunction;
+
+  @Ref() readonly inputField!: HTMLInputElement;
+
+  @Watch("syncedValue")
+  private valueChanged(newValue: Object | String) {
+    this.searchText = this.getDisplayName(newValue);
+  }
+
+  mounted() {
+    this.searchText = this.getDisplayName(this.syncedValue);
+  }
+
+  private get matchingOptions(): Array<Object | String> {
+    if (!this.searchText) return this.options;
+    return this.filterOptions(this.searchText, this.options);
+  }
+
+  private inputFocussed(): void {
+    this.inputField.select();
+    this.highlightIndex = 0;
+    this.showOptions = true;
+  }
+
+  private inputBlurred(): void {
+    this.showOptions = false;
+    if (this.exactMatch && this.autoSelectOnBlur) {
+      const selected = this.selectHighlighted();
+      // If nothing was selected, it means the search text does not match a
+      // valid option. So reset the search text to the old value.
+      if (!selected) this.searchText = this.getDisplayName(this.syncedValue);
+    }
+  }
+
+  private highlightDown(): void {
+    if (!this.showOptions) {
+      this.showOptions = true;
+    } else if (this.highlightIndex < this.matchingOptions.length - 1) {
+      ++this.highlightIndex;
+    }
+  }
+
+  private highlightUp(): void {
+    if (!this.showOptions) {
+      this.showOptions = true;
+    } else if (this.highlightIndex > 0) {
+      --this.highlightIndex;
+    }
+  }
+
+  private searchTextChanged(): void {
+    this.showOptions = true;
+    this.highlightIndex = 0;
+  }
+
+  private handleInputEnter(): void {
+    if (!this.showOptions) this.showOptions = true;
+    else this.selectHighlighted();
+  }
+
+  /**
+   * Selects the highlighted option and returns true.
+   * If there is no highlighted option, does nothing and returns false.
+   */
+  private selectHighlighted(): boolean {
+    if (this.matchingOptions.length > this.highlightIndex) {
+      this.optionSelected(this.matchingOptions[this.highlightIndex]);
+      return true;
+    }
+    return false;
+  }
+
+  private optionSelected(option: Object | String): void {
+    this.searchText = this.getDisplayName(option);
+    this.syncedValue = option;
+    this.showOptions = false;
+  }
+
+  private searchText = "";
+  private showOptions = false;
+  private highlightIndex = 0;
+}
+
+function defaultGetDisplayName(option: Object | String): string {
+  return option.toLocaleString();
+}
+
+function defaultFilterOptions(
+  searchText: string,
+  options: Array<Object | String>
+): Array<Object | String> {
+  const searchTextLowerCase = searchText.toLowerCase();
+  return options.filter((option: Object | String) =>
+    option
+      .toString()
+      .toLowerCase()
+      .includes(searchTextLowerCase)
+  );
+}
+
+function defaultCreateOption(input: string): Object | String {
+  return input;
+}
+</script>
+
+<style lang="scss" scoped>
+$border-radius: 5px;
+$border-width: 1px;
+$font-size: 14px;
+$line-height-multiplier: 1.8;
+$padding: 5px;
+$primary-color: gray;
+
+.tree-ware-select {
+  box-sizing: content-box;
+  display: inline-block;
+  font-size: $font-size;
+  line-height: $line-height-multiplier;
+  position: relative;
+
+  input {
+    border: $border-width solid $primary-color;
+    border-radius: $border-radius;
+    box-sizing: border-box;
+    font-size: $font-size;
+    line-height: $line-height-multiplier;
+    padding: $padding;
+    width: 100%;
+  }
+
+  .tree-ware-select-options {
+    border: $border-width solid $primary-color;
+    border-bottom-left-radius: $border-radius;
+    border-bottom-right-radius: $border-radius;
+    border-collapse: collapse;
+    border-top: none;
+    display: none;
+    line-height: $line-height-multiplier;
+    max-height: 200px;
+    position: absolute;
+    table-layout: fixed;
+    width: 100%;
+    z-index: 2;
+
+    .tree-ware-select-option-row {
+      td {
+        padding: $padding;
+      }
+
+      .display-name {
+        width: 100%;
+      }
+
+      &.highlight {
+        background-color: $primary-color;
+      }
+      &:hover {
+        background-color: $primary-color;
+        cursor: pointer;
+      }
+    }
+  }
+}
+
+.show-options {
+  input {
+    border-bottom-left-radius: 0px;
+    border-bottom-right-radius: 0px;
+  }
+
+  .tree-ware-select-options {
+    display: block;
+  }
+}
+</style>
