6a53b806aceac8d8142941f6f34ed4057aec3051
Deepak Nulu <4168103+deepak-nulu@users.noreply.github.com>
2019-11-26 09:24:15 -0800

    Tabs component for wrapping <router-link> tags

---
+++ src/components/TreeWareTabs.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="tree-ware-tabs flex flex-row">
+    <slot></slot>
+  </div>
+</template>
+
+<script lang="ts">
+import "reflect-metadata";
+import { Component, Vue } from "vue-property-decorator";
+
+/** Wraps <router-link> tags to make them look like tabs. */
+@Component
+export default class TreeWareTabs extends Vue {}
+</script>
+
+<style lang="scss" scoped>
+$primary-color: lightgray;
+$secondary-color: gray;
+
+.tree-ware-tabs {
+  a {
+    color: $secondary-color;
+    padding: 1rem;
+
+    &.router-link-active {
+      color: $primary-color;
+      cursor: default;
+      border-bottom: solid 1px $primary-color;
+    }
+  }
+}
+</style>
