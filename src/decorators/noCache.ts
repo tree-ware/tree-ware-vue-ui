d50fe6aab40b0bcd3913566eaa4607f0007ba434
Deepak Nulu <4168103+deepak-nulu@users.noreply.github.com>
2020-01-11 22:31:19 -0800

    NoCache decorator

---
+++ src/decorators/noCache.ts
@@ -0,0 +1,9 @@
+import { ComputedOptions } from 'vue'
+import { createDecorator } from 'vue-class-component'
+
+export const NoCache = createDecorator((options, key) => {
+    if (options.computed) {
+        let computedOptions = options.computed[key] as ComputedOptions<any>
+        computedOptions.cache = false
+    }
+})
