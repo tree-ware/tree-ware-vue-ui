8a70f50698916a11b869ea0214b8e1d021c80cac
Deepak Nulu <4168103+deepak-nulu@users.noreply.github.com>
2020-02-28 14:36:38 -0800

    Split-pane component

---
+++ src/components/TreeWareSplitPane.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="tree-ware-split-pane flex flex-row">
+    <div v-if="hasFirst" class="flex flex-1">
+      <slot name="first"></slot>
+    </div>
+    <div v-if="hasSecond" class="flex flex-1">
+      <slot name="second"></slot>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import "reflect-metadata";
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+@Component
+export default class TreeWareSplitPane extends Vue {
+  @Prop({ default: false }) readonly hasFirst!: boolean;
+  @Prop({ default: false }) readonly hasSecond!: boolean;
+}
+</script>
