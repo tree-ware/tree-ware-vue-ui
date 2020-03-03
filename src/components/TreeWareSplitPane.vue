a2739f83f73d15f18aa00c2ae98500286fe1eb9c
Deepak Nulu <4168103+deepak-nulu@users.noreply.github.com>
2020-03-03 15:02:43 -0800

    CSS class properties for panes in the split-pane component

--- src/components/TreeWareSplitPane.vue
+++ src/components/TreeWareSplitPane.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="tree-ware-split-pane flex flex-row">
-    <div v-if="hasFirst" class="flex flex-1">
+    <div v-if="hasFirst" :class="firstClasses">
       <slot name="first"></slot>
     </div>
-    <div v-if="hasSecond" class="flex flex-1">
+    <div v-if="hasSecond" :class="secondClasses">
       <slot name="second"></slot>
     </div>
   </div>
@@ -17,5 +17,8 @@ import { Component, Prop, Vue } from "vue-property-decorator";
 export default class TreeWareSplitPane extends Vue {
   @Prop({ default: false }) readonly hasFirst!: boolean;
   @Prop({ default: false }) readonly hasSecond!: boolean;
+
+  @Prop({ default: "" }) readonly firstClasses!: string;
+  @Prop({ default: "" }) readonly secondClasses!: string;
 }
 </script>
