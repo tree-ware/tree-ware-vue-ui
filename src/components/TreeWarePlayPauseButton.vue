56be46fd92e0de795785bac9029fe2355ac1557a
Deepak Nulu <4168103+deepak-nulu@users.noreply.github.com>
2020-07-03 13:57:55 -0700

    Implement a play/pause component that emits a periodic tick

---
+++ src/components/TreeWarePlayPauseButton.vue
@@ -0,0 +1,55 @@
+<template>
+  <vs-button @click="toggle()" icon-pack="fas" :icon="getIcon()" :color="getColor()" size="large" />
+</template>
+
+<script lang="ts">
+import "reflect-metadata";
+import {
+  Component,
+  Emit,
+  Prop,
+  PropSync,
+  Vue,
+  Watch
+} from "vue-property-decorator";
+
+@Component
+export default class TreeWarePlayPauseButton extends Vue {
+  @Prop({ default: 5 }) readonly tickIntervalSeconds!: number;
+  @PropSync("play", { type: Boolean }) syncedPlay!: Boolean;
+
+  @Emit() tick() {}
+
+  @Watch("syncedPlay")
+  playChanged(newPlay: Boolean, oldPlay: Boolean) {
+    if (newPlay) this.startTimer();
+    else this.clearTimer();
+  }
+
+  private startTimer() {
+    this.clearTimer();
+    this.timerId = setInterval(this.tick, this.tickIntervalSeconds * 1000);
+  }
+
+  private clearTimer() {
+    if (this.timerId !== undefined) {
+      clearInterval(this.timerId);
+      this.timerId = undefined;
+    }
+  }
+
+  private toggle() {
+    this.syncedPlay = !this.syncedPlay;
+  }
+
+  private getIcon(): string {
+    return this.syncedPlay ? "fa-pause" : "fa-play";
+  }
+
+  private getColor(): string {
+    return this.syncedPlay ? "warning" : "primary";
+  }
+
+  private timerId?: number;
+}
+</script>
