93f7699c8a88af717b69751b172d2eb8b207d920
Deepak Nulu <4168103+deepak-nulu@users.noreply.github.com>
2020-12-16 12:47:03 -0800

    Add utilities for building network graph data structures

---
+++ src/components/TreeWareNetworkGraphUtil.ts
@@ -0,0 +1,28 @@
+import { Graph, Link, Node } from './TreeWareNetworkGraphInterfaces'
+
+export function addNode<N, L>(
+  node: Node<N>,
+  graph: Graph<N, L>,
+  nodeIdSet: Set<string>
+): boolean {
+  if (nodeIdSet.has(node.id)) return false
+  nodeIdSet.add(node.id)
+  graph.nodes.push(node)
+  return true
+}
+
+export function addLink<N, L>(
+  link: Link<L>,
+  graph: Graph<N, L>,
+  linkIdSet: Set<string>
+): boolean {
+  const linkId = getId(link.sourceId, link.targetId, link.linkType)
+  if (linkIdSet.has(linkId)) return false
+  linkIdSet.add(linkId)
+  graph.links.push(link)
+  return true
+}
+
+export function getId(...parts: string[]): string {
+  return parts.join('-')
+}
