69823ce37adbc595b98aa74f17f4f9875f2b7930
Deepak Nulu <4168103+deepak-nulu@users.noreply.github.com>
2021-01-28 09:43:01 -0800

    Add a component for displaying & clearing counts

---
+++ src/components/ClearableCount.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="flex flex-row items-center">
+    <span class="mr-2">{{ label }}:</span>
+    <vs-chip
+      v-if="count > 0"
+      @click="clear"
+      closable
+      color="warning"
+      class="whitespace-no-wrap text-base mb-0"
+    >
+      {{ count }}
+    </vs-chip>
+    <span v-else>0</span>
+  </div>
+</template>
+
+<script lang="ts">
+import 'reflect-metadata'
+import { Component, Emit, Prop, Vue } from 'vue-property-decorator'
+
+@Component
+export default class ClearableCount extends Vue {
+  @Prop() readonly label!: string
+  @Prop() readonly count!: number
+  @Emit() clear() {}
+}
+</script>
