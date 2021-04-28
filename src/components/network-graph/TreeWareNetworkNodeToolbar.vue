b637938919623ebb871ad69ae04d704857154b81
Deepak Nulu <4168103+deepak-nulu@users.noreply.github.com>
2021-04-27 18:00:11 -0700

    Add a slot for custom tools in the node toolbar

--- src/components/network-graph/TreeWareNetworkNodeToolbar.vue
+++ src/components/network-graph/TreeWareNetworkNodeToolbar.vue
@@ -1,67 +1,78 @@
 <template>
   <div class="tree-ware-network-node-toolbar">
-    <div class="toolbar flex flex-row">
-      <vx-tooltip
-        v-if="node.canPin"
-        :text="userControlTooltip.canPin"
-        :class="['pin', { 'is-pinned': node.isPinned }, 'mr-2']"
-      >
-        <a @click="pinClick(node, !node.isPinned)">
-          <vs-icon
-            icon="fa-thumbtack"
-            icon-pack="fas"
-            :color="node.isPinned ? pinnedPinColor : 'primary'"
-          ></vs-icon>
-        </a>
-      </vx-tooltip>
+    <div class="flex flex flex-row items-center justify-between">
+      <div class="toolbar flex flex-row">
+        <vx-tooltip
+          v-if="node.canPin"
+          :text="userControlTooltip.canPin"
+          :class="['pin', { 'is-pinned': node.isPinned }, 'mr-2']"
+        >
+          <a @click="pinClick(node, !node.isPinned)">
+            <vs-icon
+              icon="fa-thumbtack"
+              icon-pack="fas"
+              :color="node.isPinned ? pinnedPinColor : 'primary'"
+            ></vs-icon>
+          </a>
+        </vx-tooltip>
 
-      <vx-tooltip
-        v-if="node.canExpand"
-        :text="userControlTooltip.canExpand"
-        class="inline mr-2"
-      >
-        <a @click="expandClick(node, !node.isExpanded)">
-          <vs-icon :icon="expandIcon" icon-pack="fas" color="primary"></vs-icon>
-        </a>
-      </vx-tooltip>
+        <vx-tooltip
+          v-if="node.canExpand"
+          :text="userControlTooltip.canExpand"
+          class="inline mr-2"
+        >
+          <a @click="expandClick(node, !node.isExpanded)">
+            <vs-icon
+              :icon="expandIcon"
+              icon-pack="fas"
+              color="primary"
+            ></vs-icon>
+          </a>
+        </vx-tooltip>
 
-      <vx-tooltip
-        v-if="node.canZoom"
-        :text="userControlTooltip.canZoom"
-        class="inline mr-2"
-      >
-        <a @click="zoomClick(node)">
-          <vs-icon
-            icon="fa-search-plus"
-            icon-pack="fas"
-            color="primary"
-          ></vs-icon>
-        </a>
-      </vx-tooltip>
+        <vx-tooltip
+          v-if="node.canZoom"
+          :text="userControlTooltip.canZoom"
+          class="inline mr-2"
+        >
+          <a @click="zoomClick(node)">
+            <vs-icon
+              icon="fa-search-plus"
+              icon-pack="fas"
+              color="primary"
+            ></vs-icon>
+          </a>
+        </vx-tooltip>
 
-      <vx-tooltip
-        v-if="node.canHide"
-        :text="userControlTooltip.canHide"
-        class="inline mr-2"
-      >
-        <a @click="hideClick(node, !node.isHidden)">
-          <vs-icon
-            icon="fa-eye-slash"
-            icon-pack="far"
-            color="primary"
-          ></vs-icon>
-        </a>
-      </vx-tooltip>
+        <vx-tooltip
+          v-if="node.canHide"
+          :text="userControlTooltip.canHide"
+          class="inline mr-2"
+        >
+          <a @click="hideClick(node, !node.isHidden)">
+            <vs-icon
+              icon="fa-eye-slash"
+              icon-pack="far"
+              color="primary"
+            ></vs-icon>
+          </a>
+        </vx-tooltip>
 
-      <vx-tooltip
-        v-if="node.canLog"
-        :text="userControlTooltip.canLog"
-        class="inline mr-2"
-      >
-        <a @click="logClick">
-          <vs-icon icon="fa-terminal" icon-pack="fas" color="primary"></vs-icon>
-        </a>
-      </vx-tooltip>
+        <vx-tooltip
+          v-if="node.canLog"
+          :text="userControlTooltip.canLog"
+          class="inline mr-2"
+        >
+          <a @click="logClick">
+            <vs-icon
+              icon="fa-terminal"
+              icon-pack="fas"
+              color="primary"
+            ></vs-icon>
+          </a>
+        </vx-tooltip>
+      </div>
+      <slot />
     </div>
 
     <vx-tooltip
