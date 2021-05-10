24d3f95893e1b74e947de0f36996a1b21ecf2268
Deepak Nulu <4168103+deepak-nulu@users.noreply.github.com>
2021-05-10 16:54:44 -0700

    Combine custom event fields in an interface

--- src/components/network-graph/useTreeWareNetworkNodeEmits.ts
+++ src/components/network-graph/useTreeWareNetworkNodeEmits.ts
@@ -3,16 +3,18 @@ import { TreeWareNetworkNode } from './TreeWareNetworkNode'
 
 export const CUSTOM_NODE_EVENT = 'custom-node-event'
 
+export interface TreeWareNetworkNodeCustomEvent {
+  name: string
+  data: any
+  source: TreeWareNetworkNode
+}
+
 export function useTreeWareNetworkNodeEmits(
   emit: SetupContext['emit'],
   usedBy: string = ''
 ) {
-  function customNodeEvent(
-    eventName: string,
-    eventData: any,
-    node: TreeWareNetworkNode
-  ) {
-    emit(CUSTOM_NODE_EVENT, eventName, eventData, node)
+  function customNodeEvent(event: TreeWareNetworkNodeCustomEvent) {
+    emit(CUSTOM_NODE_EVENT, event)
   }
 
   return { customNodeEvent }
